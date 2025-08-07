58f2571fd41e7c0d83e4a84108e2dbc42608ce21
adejazzmind <adejazzmind@gmail.com>
2025-08-07 02:54:28 -0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LOANAPP3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            double AMOUNT = int.Parse(textBox1.Text);
+            double DURATION = int.Parse(textBox2.Text);
+            double INTEREST = AMOUNT * 0.1;
+            double PAYMENT = (AMOUNT + INTEREST) / DURATION;
+            label3.Text = PAYMENT.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
